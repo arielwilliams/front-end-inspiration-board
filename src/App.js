5bb2429989b3335c8564e914698df29509c277bc
Ariel Williams <arielwilliams121013@gmail.com>
2023-06-27 11:59:12 -0700

    Created additional components. Created useState in App.js. Worked with Edith.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
+// Edith and Ariel left off here. Created two useState ... 
 function App() {
+  const [boardsData, setBoardsData] = useState([]);
+  const [selectedBoard, setselectedBoard]  = useState({
+    title: '',
+    owner: '',
+    board_id: ''
+
+});
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,3 +35,28 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// What we started with in App.js
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
